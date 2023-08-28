15e67d2c062561e6d36ce9bee3effebdddc82568
Shiro <matias_shiroma@hotmail.com>
2023-08-28 01:16:20 -0300

    Modificaciones de HTML y CSS

--- js/test.js
+++ js/test.js
@@ -1,26 +1,22 @@
-console.log('Hola mundo');
+const sueldoBruto = 114500;
+const tasaInflacionMensual = 0.063; // 6.3% en decimal
+const tipoCambioDolar = 730;
+const costoDeVidaEnDolares = 1000; // Establece el costo de vida estimado en dólares
 
-//Variables
-let mi_primera_var = ('Mi primera variable en js!');
-console.log(mi_primera_var);
+// Calculamos el sueldo ajustado por inflación
+function calcularSueldoAjustado(sueldo, tasaInflacion) {
+  return sueldo * Math.pow(1 + tasaInflacion, 12); // Multiplicamos por 12 para ajustar a un año
+}
 
-mi_primera_var = 'Esto a cambiado!';
-console.log(mi_primera_var);
+const sueldoAjustado = calcularSueldoAjustado(sueldoBruto, tasaInflacionMensual);
 
+// Convertimos el costo de vida estimado de dólares a pesos
+const costoDeVidaEnPesos = costoDeVidaEnDolares * tipoCambioDolar;
 
-//boolean
-let esMayorDeEdad = true;
-let tienePermiso = false;
-console.log(esMayorDeEdad);
+// Verificamos si el sueldo ajustado es suficiente para cubrir el costo de vida
+const esSuficiente = sueldoAjustado >= costoDeVidaEnPesos;
 
-
-//Null
-let resultado = null;
-console.log(resultado);
-
-
-//Undefined
-let nombre;
-console.log(nombre); // Output: undefined
-
-
+// Mostramos el resultado
+console.log(`Sueldo ajustado por inflación: $${sueldoAjustado.toFixed(2)}`);
+console.log(`Costo de vida estimado en pesos: $${costoDeVidaEnPesos.toFixed(2)}`);
+console.log(`¿El sueldo es suficiente? ${esSuficiente ? 'Sí' : 'No'}`);
